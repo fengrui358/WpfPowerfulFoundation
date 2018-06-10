5c28a01f977c436a672827f6c1276ea2879f3714
free <devqj@qq.com>
2018-06-11 01:54:42 +0800

    测试

--- Core/ViewModels/FirstViewModel.cs
+++ Core/ViewModels/FirstViewModel.cs
@@ -1,4 +1,5 @@
-﻿using FrHello.NetLib.Core.Mvx;
+﻿using System.Threading.Tasks;
+using FrHello.NetLib.Core.Mvx;
 using Models;
 using MvvmCross.Commands;
 
@@ -10,11 +11,21 @@ namespace Core.ViewModels
 
         public MvxCommand GetInput1Command { get; private set; }
 
+        /// <summary>
+        /// 数据变化会自动改变
+        /// </summary>
+        public bool IsChanged { get; set; }
+
         public FirstViewModel()
         {
-            TestModel = new TestModel();
+            Task.Run(async () =>
+            {
+                await Task.Delay(5000);
+                TestModel = new TestModel();
+            });
 
-            GetInput1Command = new MvxCommand(GetInput1CommandHandler);
+            
+            GetInput1Command = new MvxCommand(GetInput1CommandHandler, () => TestModel != null);
         }
 
         private void GetInput1CommandHandler()
