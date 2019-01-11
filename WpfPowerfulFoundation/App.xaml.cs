8cc142621ea86930fc3b00db90760aeb2c332af2
free <devqj@qq.com>
2019-01-10 23:26:00 -0400

    初始化服务工具程序集

--- WpfPowerfulFoundation/App.xaml.cs
+++ WpfPowerfulFoundation/App.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
 using Core.ViewModels;
 using FrHello.NetLib.Core.Wpf;
 using WpfViews.Windows;
@@ -11,12 +16,31 @@ namespace WpfPowerfulFoundation
     public partial class App
     {
         /// <summary>
+        /// 构造
+        /// </summary>
+        public App()
+        {
+            MvxHelper.CaptureGlobalExceptions();
+        }
+
+        /// <summary>
         /// 启动
         /// </summary>
         /// <param name="e"></param>
         protected override void OnStartup(StartupEventArgs e)
         {
-            MvxHelper.Init<MainWindowView, FirstViewModel>();
+            var serviceAssembliesPathList = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "*Service.dll",
+                SearchOption.TopDirectoryOnly);
+            var serviceAssemblies = new List<Assembly>();
+            if (serviceAssembliesPathList.Any())
+            {
+                foreach (var path in serviceAssembliesPathList)
+                {
+                    serviceAssemblies.Add(Assembly.LoadFile(path));
+                }
+            }
+
+            MvxHelper.Init<MainWindowView, FirstViewModel>(serviceAssemblies: serviceAssemblies.ToArray());
         }
     }
 }
