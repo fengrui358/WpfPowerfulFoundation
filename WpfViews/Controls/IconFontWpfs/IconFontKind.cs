0077090f91ce7e2176e79b08d2fb2c1b733c9c01
fengrui@ceiec.com.cn <fengrui@ceiec.com.cn>
2019-05-28 09:13:34 +0800

    增加矢量文字图片工具

--- WpfViews/Controls/IconFontWpfs/IconFontKind.cs
+++ WpfViews/Controls/IconFontWpfs/IconFontKind.cs
@@ -11,11 +11,306 @@ namespace WpfViews.Controls.IconFontWpfs
         None,
 
         /// <summary>
-        /// 中文描述
+        /// 尺子
+        /// cemian
+        /// Class/cemian
+        /// </summary>
+        [Description("尺子 cemian")]
+        Chizi,
+
+        /// <summary>
+        /// 放大镜
+        /// ssuoguanli
+        /// Class/ssuoguanli
+        /// </summary>
+        [Description("放大镜 ssuoguanli")]
+        fangdajing,
+
+        /// <summary>
+        /// cexian
+        /// Class/cexian
+        /// </summary>
+        [Description("cexian")]
+        cexian,
+
+        /// <summary>
+        /// nav2icon
+        /// Class/navicon
+        /// </summary>
+        [Description("nav2icon/navicon")]
+        navicon,
+
+        /// <summary>
+        /// nav1icon
+        /// Class/navicon1
+        /// </summary>
+        [Description("nav1icon/navicon1")]
+        navicon1,
+
+        /// <summary>
+        /// bozhuogongju
+        /// Class/bozhuogongju
+        /// </summary>
+        [Description("bozhuogongju")]
+        bozhuogongju,
+
+        /// <summary>
+        /// Tucengguanli
+        /// Class/Tucengguanli
+        /// </summary>
+        [Description("Tucengguanli")]
+        Tucengguanli,
+
+        /// <summary>
+        /// na3icon
+        /// Class/naicon
+        /// </summary>
+        [Description("na3icon/naicon")]
+        naicon,
+
+        /// <summary>
+        /// logo
+        /// Class/logo
+        /// </summary>
+        [Description("logo")]
+        logo,
+
+        /// <summary>
+        /// guanbi
+        /// Class/guanbi
+        /// </summary>
+        [Description("guanbi")]
+        guanbi,
+
+        /// <summary>
+        /// zuixiaohua
+        /// Class/zuixiaohua
+        /// </summary>
+        [Description("zuixiaohua")]
+        zuixiaohua,
+
+        /// <summary>
+        /// zuidahua
+        /// Class/zuidahua
+        /// </summary>
+        [Description("zuidahua")]
+        zuidahua,
+
+        /// <summary>
+        /// lujing
+        /// Class/lujing
+        /// </summary>
+        [Description("lujing")]
+        lujing,
+
+        /// <summary>
+        /// chongzuo
+        /// Class/chongzuo
+        /// </summary>
+        [Description("chongzuo")]
+        chongzuo,
+
+        /// <summary>
+        /// chexiao
+        /// Class/chexiao
+        /// </summary>
+        [Description("chexiao")]
+        chexiao,
+
+        /// <summary>
+        /// paixu
+        /// Class/paixu
+        /// </summary>
+        [Description("paixu")]
+        paixu,
+
+        /// <summary>
+        /// yincang
+        /// Class/yincang
+        /// </summary>
+        [Description("yincang")]
+        yincang,
+
+        /// <summary>
+        /// plus
+        /// Class/plus
+        /// </summary>
+        [Description("plus")]
+        plus,
+
+        /// <summary>
+        /// footerdelete
+        /// Class/footerdelete
+        /// </summary>
+        [Description("footerdelete")]
+        footerdelete,
+
+        /// <summary>
+        /// footerexport
+        /// Class/footerexport
+        /// </summary>
+        [Description("footerexport")]
+        footerexport,
+
+        /// <summary>
+        /// footersave
+        /// Class/footersave
+        /// </summary>
+        [Description("footersave")]
+        footersave,
+
+        /// <summary>
+        /// tool_delete
+        /// Class/tool_delete
+        /// </summary>
+        [Description("tool_delete")]
+        tool_delete,
+
+        /// <summary>
+        /// tool_combine
+        /// Class/tool_combine
+        /// </summary>
+        [Description("tool_combine")]
+        tool_combine,
+
+        /// <summary>
+        /// tool_edit
+        /// Class/tool_edit
+        /// </summary>
+        [Description("tool_edit")]
+        tool_edit,
+
+        /// <summary>
+        /// tool_move
+        /// Class/tool_move
+        /// </summary>
+        [Description("tool_move")]
+        tool_move,
+
+        /// <summary>
+        /// tool_fanxiang
+        /// Class/tool_fanxiang1
+        /// </summary>
+        [Description("tool_fanxiang/tool_fanxiang1")]
+        tool_fanxiang1,
+
+        /// <summary>
+        /// tool_cut
+        /// Class/tool_cut
+        /// </summary>
+        [Description("tool_cut")]
+        tool_cut,
+
+        /// <summary>
+        /// tool_rotate
+        /// Class/tool_rotate
+        /// </summary>
+        [Description("tool_rotate")]
+        tool_rotate,
+
+        /// <summary>
+        /// icon_1
+        /// Class/icon_
+        /// </summary>
+        [Description("icon_1/icon_")]
+        icon_,
+
+        /// <summary>
+        /// icon_7
+        /// Class/icon_1
+        /// </summary>
+        [Description("icon_7/icon_1")]
+        icon_1,
+
+        /// <summary>
+        /// icon_2
+        /// Class/icon_2
+        /// </summary>
+        [Description("icon_2")]
+        icon_2,
+
+        /// <summary>
+        /// icon_5
+        /// Class/icon_3
+        /// </summary>
+        [Description("icon_5/icon_3")]
+        icon_3,
+
+        /// <summary>
+        /// icon_3
+        /// Class/icon_4
+        /// </summary>
+        [Description("icon_3/icon_4")]
+        icon_4,
+
+        /// <summary>
+        /// icon_4
+        /// Class/icon_5
+        /// </summary>
+        [Description("icon_4/icon_5")]
+        icon_5,
+
+        /// <summary>
+        /// icon_6
+        /// Class/icon_6
+        /// </summary>
+        [Description("icon_6")]
+        icon_6,
+
+        /// <summary>
+        /// icon_selected
+        /// Class/icon_selected
+        /// </summary>
+        [Description("icon_selected")]
+        icon_selected,
+
+        /// <summary>
+        /// huidian
+        /// Class/huidian
+        /// </summary>
+        [Description("huidian")]
+        huidian,
+
+        /// <summary>
+        /// uploadicon
+        /// Class/uploadicon
+        /// </summary>
+        [Description("uploadicon")]
+        uploadicon,
+
+        /// <summary>
+        /// icon_selected
+        /// Class/icon_selected1
+        /// </summary>
+        [Description("icon_selected/icon_selected1")]
+        icon_selected1,
+
+        /// <summary>
         /// selectfiles
         /// Class/selectfiles
         /// </summary>
-        [Description("中文描述 selectfiles/selectfiles")]
-        Gear
+        [Description("selectfiles")]
+        selectfiles,
+
+        /// <summary>
+        /// xuanzhuan
+        /// Class/xuanzhuan
+        /// </summary>
+        [Description("xuanzhuan")]
+        xuanzhuan,
+
+        /// <summary>
+        /// kejian
+        /// Class/kejian
+        /// </summary>
+        [Description("kejian")]
+        kejian,
+
+        /// <summary>
+        /// huimian
+        /// Class/huimian
+        /// </summary>
+        [Description("huimian")]
+        huimian
     }
 }
---
+++ WpfViews/Resources/IconFont/IconFont.bat
@@ -0,0 +1,1 @@
+..\..\..\Tools\IconFontTool\IconFontTool.exe -factoryPath "..\..\Controls\IconFontWpfs\IconFontDataFactory.cs" -kindPath "..\..\Controls\IconFontWpfs\IconFontKind.cs" -iconFilePath "iconfont.ttf" -mappingFilePath "IconFontMapping.txt" -factoryNameSpace "WpfViews.Controls.IconFontWpfs" -kindNameSpace "WpfViews.Controls.IconFontWpfs" -iconPrefix "icon"
